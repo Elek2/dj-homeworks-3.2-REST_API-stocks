18027f6125be15df275f75c97916b3f3e0221087
Elek2 <104815402+Elek2@users.noreply.github.com>
2023-05-13 11:27:35 +0300

    Update urls.py

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from logistic.views import ProductViewSet, StockViewSet, Hello
+from logistic.views import ProductViewSet, StockViewSet, FinalChangesGitworkFlow
 
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
 
-urlpatterns = [path('', Hello.as_view())]+router.urls
+urlpatterns = [path('', FinalChangesGitworkFlow.as_view())]+router.urls
 
 # SDF
